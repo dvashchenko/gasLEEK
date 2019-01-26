1b7917800470254ebab9f12970fc823f57cb970a
Adrian Darian <adarian@ucmerced.edu>
2019-01-25 19:14:30 -0800

    working model of deck.gl

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,33 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {StaticMap} from 'react-map-gl';
+import DeckGL, {LineLayer, ScatterplotLayer} from 'deck.gl';
+
+// Set your mapbox token here
+const MAPBOX_TOKEN = 'pk.eyJ1IjoiYWRhcmlhbiIsImEiOiJjanJjdzlqdTYwOGc3NDlxcXU3M291cmx1In0.fceqY9ssXVrQxPh0ZJFXRw';
+
+const INITIAL_VIEW_STATE = {
+  latitude: 37.785164,
+  longitude: -122.41669,
+  zoom: 16,
+  bearing: -20,
+  pitch: 60
+};
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <DeckGL initialViewState={INITIAL_VIEW_STATE} controller={true} width="100%" height="100%">
+        <StaticMap mapboxApiAccessToken={MAPBOX_TOKEN} />
+        <LineLayer
+          data={[{sourcePosition: [-122.41669, 37.7883], targetPosition: [-122.41669, 37.781]}]}
+          strokeWidth={5}
+        />
+        <ScatterplotLayer
+          data={[{position: [-122.41669, 37.79]}]}
+          radiusScale={100}
+          getColor={x => [0, 0, 255]}
+        />
+      </DeckGL>
     );
   }
 }
