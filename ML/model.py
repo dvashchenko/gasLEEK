73359ba2955237d2b91e411726a898d78df8026e
Denis Vashchenko <denisvashchenko@gmail.com>
2019-01-26 05:38:56 -0800

    Stationary and Differencing dataset

--- ML/model.py
+++ ML/model.py
@@ -4,6 +4,7 @@ from pandas import read_csv
 from pandas import datetime
 from pandas import DataFrame
 from pandas import concat
+from pandas import Series
 from keras.models import Sequential
 from keras.layers import Dense, LSTM
 from keras.wrappers.scikit_learn import KerasRegressor
@@ -63,4 +64,35 @@ series = read_csv(path, header=0,
 # transform to supervised learning
 X = series.values
 supervised = timeseries_to_supervised(X, 1)
-print(supervised.head())
+print(supervised)
+
+# create a differenced series
+def difference(dataset, interval=1):
+    diff = list()
+    for i in range(interval, len(dataset)):
+        value = dataset[i] - dataset[i - interval]
+        diff.append(value)
+    return Series(diff)
+
+# invert differenced value
+def inverse_difference(history, yhat, interval=1):
+    return yhat + history[-interval]
+
+# load dataset
+def parser(x):
+    return datetime.strptime('190'+x, '%Y-%m')
+
+
+series = read_csv(path, header=0,
+                  parse_dates=[0], index_col=0, squeeze=True)
+print(series.head())
+# transform to be stationary
+differenced = difference(series, 1)
+print(differenced.head())
+# invert transform
+inverted = list()
+for i in range(len(differenced)):
+    value = inverse_difference(series, differenced[i], len(series)-i)
+    inverted.append(value)
+inverted = Series(inverted)
+print(inverted.head())
