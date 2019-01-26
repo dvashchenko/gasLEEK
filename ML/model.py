753520cfa6aed53b9c979d5a8f9c12ee471c8f35
Denis Vashchenko <denisvashchenko@gmail.com>
2019-01-26 06:28:42 -0800

    Completion of LSTM model

--- ML/model.py
+++ ML/model.py
@@ -67,6 +67,8 @@ supervised = timeseries_to_supervised(X, 1)
 print(supervised)
 
 # create a differenced series
+
+
 def difference(dataset, interval=1):
     diff = list()
     for i in range(interval, len(dataset)):
@@ -75,12 +77,37 @@ def difference(dataset, interval=1):
     return Series(diff)
 
 # invert differenced value
+
+
 def inverse_difference(history, yhat, interval=1):
     return yhat + history[-interval]
 
 # load dataset
-def parser(x):
-    return datetime.strptime('190'+x, '%Y-%m')
+
+# scale train and test data to [-1, 1]
+
+
+def scale(train, test):
+        # fit scaler
+    scaler = MinMaxScaler(feature_range=(-1, 1))
+    scaler = scaler.fit(train)
+    # transform train
+    train = train.reshape(train.shape[0], train.shape[1])
+    train_scaled = scaler.transform(train)
+    # transform test
+    test = test.reshape(test.shape[0], test.shape[1])
+    test_scaled = scaler.transform(test)
+    return scaler, train_scaled, test_scaled
+
+# inverse scaling for a forecasted value
+
+
+def invert_scale(scaler, X, value):
+    new_row = [x for x in X] + [value]
+    array = np.array(new_row)
+    array = array.reshape(1, len(array))
+    inverted = scaler.inverse_transform(array)
+    return inverted[0, -1]
 
 
 series = read_csv(path, header=0,
@@ -96,3 +123,86 @@ for i in range(len(differenced)):
     inverted.append(value)
 inverted = Series(inverted)
 print(inverted.head())
+
+# transform scale
+X = series.values
+X = X.reshape(len(X), 1)
+scaler = MinMaxScaler(feature_range=(-1, 1))
+scaler = scaler.fit(X)
+scaled_X = scaler.transform(X)
+scaled_series = Series(scaled_X[:, 0])
+print(scaled_series.head())
+
+# invert transform
+inverted_X = scaler.inverse_transform(scaled_X)
+inverted_series = Series(inverted_X[:, 0])
+print(inverted_series.head())
+
+# MODEL
+
+
+def fit_lstm(train, batch_size, nb_epoch, neurons):
+    X, y = train[:, 0:-1], train[:, -1]
+    X = X.reshape(X.shape[0], 1, X.shape[1])
+    model = Sequential()
+    model.add(LSTM(neurons, batch_input_shape=(
+        batch_size, X.shape[1], X.shape[2]), stateful=True))
+    model.add(Dense(1))
+    model.compile(loss='mean_squared_error', optimizer='adam')
+    for i in range(nb_epoch):
+        model.fit(X, y, epochs=1, batch_size=batch_size,
+                  verbose=0, shuffle=False)
+        model.reset_states()
+    return model
+
+
+def forecast(model, batch_size, row):
+    X = row[0:-1]
+    X = X.reshape(1, 1, len(X))
+    yhat = model.predict(X, batch_size=batch_size)
+    return yhat[0, 0]
+
+
+series = read_csv(path, header=0, parse_dates=[0], index_col=0, squeeze=True)
+# transform data to be stationary
+raw_values = series.values
+diff_values = difference(raw_values, 1)
+
+# transform data to be supervised learning
+supervised = timeseries_to_supervised(diff_values, 1)
+supervised_values = supervised.values
+
+# split data into train and test-sets
+train, test = supervised_values[0:-12], supervised_values[-12:]
+
+# transform the scale of the data
+scaler, train_scaled, test_scaled = scale(train, test)
+
+# fit the model
+lstm_model = fit_lstm(train_scaled, 1, 3000, 4)
+# forecast the entire training dataset to build up state for forecasting
+train_reshaped = train_scaled[:, 0].reshape(len(train_scaled), 1, 1)
+lstm_model.predict(train_reshaped, batch_size=1)
+
+# walk-forward validation on the test data
+predictions = list()
+for i in range(len(test_scaled)):
+	# make one-step forecast
+	X, y = test_scaled[i, 0:-1], test_scaled[i, -1]
+	yhat = forecast_lstm(lstm_model, 1, X)
+	# invert scaling
+	yhat = invert_scale(scaler, X, yhat)
+	# invert differencing
+	yhat = inverse_difference(raw_values, yhat, len(test_scaled)+1-i)
+	# store forecast
+	predictions.append(yhat)
+	expected = raw_values[len(train) + i + 1]
+	print('Month=%d, Predicted=%f, Expected=%f' % (i+1, yhat, expected))
+
+# report performance
+rmse = sqrt(mean_squared_error(raw_values[-12:], predictions))
+print('Test RMSE: %.3f' % rmse)
+# line plot of observed vs predicted
+pyplot.plot(raw_values[-12:])
+pyplot.plot(predictions)
+pyplot.show()
