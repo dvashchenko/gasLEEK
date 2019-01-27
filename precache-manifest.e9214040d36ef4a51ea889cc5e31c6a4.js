96ab807e8eae93f308cb33a2ceecfd0237d77e9d
Adrian Darian <adarian@ucmerced.edu>
2019-01-26 17:57:34 -0800

    Updates

---
+++ precache-manifest.e9214040d36ef4a51ea889cc5e31c6a4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5e33c6875342a939c5a",
+    "url": "/gasLEEK/static/css/main.00aaff28.chunk.css"
+  },
+  {
+    "revision": "b5e33c6875342a939c5a",
+    "url": "/gasLEEK/static/js/main.b5e33c68.chunk.js"
+  },
+  {
+    "revision": "878f9cfee56e8647e627",
+    "url": "/gasLEEK/static/js/1.878f9cfe.chunk.js"
+  },
+  {
+    "revision": "9f958e1563f26b9c2ee5",
+    "url": "/gasLEEK/static/js/runtime~main.9f958e15.js"
+  },
+  {
+    "revision": "ac6ea0937a7e0d9d4ecdc2e29eb30d5d",
+    "url": "/gasLEEK/static/media/gasLEEK.ac6ea093.png"
+  },
+  {
+    "revision": "bd6820f1a4015e9645e470ea8e7b1501",
+    "url": "/gasLEEK/static/media/anime3.bd6820f1.png"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/gasLEEK/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/gasLEEK/static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/gasLEEK/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/gasLEEK/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "2b1cf0316cf2bde1e68f5fe99a818b2c",
+    "url": "/gasLEEK/index.html"
+  }
+];
